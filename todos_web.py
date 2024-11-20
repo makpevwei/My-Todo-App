3b66583dad08361fb263ebdc30b91917c35649ad
makpevwei <makpevwei@yahoo.com>
2024-11-20 17:42:21 +0100

    Removed <b> tag from subheader

--- todos_web.py
+++ todos_web.py
@@ -17,7 +17,7 @@ def add_todo():
 st.title("My Todo App")
 
 # Add a subheader with HTML formatting for emphasis
-st.subheader("This app is to increase your <b>productivity")
+st.subheader("This app is to increase your productivity")
 
 # Display a brief description of the app's functionality
 st.write("Click on the checkbox to remove a todo.")
